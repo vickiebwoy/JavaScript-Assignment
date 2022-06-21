9e6452d30bb2d922e6243ed417296a0ee2dafda4
vickiebwoy <106430081+vickiebwoy@users.noreply.github.com>
2022-06-21 16:19:46 +0100

    Add files via upload

---
+++ JavaScript Assignment/JavaScript.js
@@ -0,0 +1,44 @@
+const myRealNameObviouslyIs = "Victor George"
+console.log(myRealNameObviouslyIs)
+
+const myNativeNameIsJust = "Tamuno Miebaka"
+console.log(myNativeNameIsJust)
+
+const myBirthNameIsOnly = "Valentine"
+console.log(myBirthNameIsOnly)
+
+const theNicknameEverybodyCallsMe = "Vickie Bwoy"
+console.log(theNicknameEverybodyCallsMe)
+
+const theNameOfMyFather = "George Jackson"
+console.log(theNameOfMyFather)
+
+const a=25
+const b=32
+const c=16
+const X=c^4*(-12*a)/b^2
+console.log(X)
+
+const d=9
+const e=22
+const f=15
+const Y=72*e^3-2*d/(f*10)
+console.log(Y)
+
+const g=4
+const h=50
+const i=25
+const Z=g*(40-12)^2*i^4/h
+console.log(Z)
+
+const j=5
+const k=120
+const l=75
+const V=(j^5-120*5)/l^2
+console.log(V)
+
+const m=100
+const n=300
+const o=500
+const W=(n-m)*(1000/o)+200
+console.log(W)
